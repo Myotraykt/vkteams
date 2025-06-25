0dd1fe7838bc6c66e1151e9308e64733a99a2fb3
Артем Иванов <myotraykt@yandex.ru>
2025-06-25 11:59:59 +0300

    add TESTCASES.md

--- pages/lobbyPage.ts
+++ pages/lobbyPage.ts
@@ -3,12 +3,10 @@ import { Locator, Page } from "@playwright/test";
 export class LobbyPage {
   readonly page: Page;
   readonly joinButton: Locator;
-  readonly backButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.joinButton = page.locator('button.im-guests-calls-submit');
-    this.backButton = page.locator('span.im-marker');
   }
 
   async joinConference() {
